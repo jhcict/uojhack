61fe7228f8960430bda532472499c214c6635584
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-12-05 06:54:45 +0530

    update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 	<meta name="author" content="">
 
 	<!-- Application Title -->
-	<title>BByer</title>
+	<title>BBuyer Web <Interface></Interface></title>
 
 	<!-- Bootstrap CSS -->
 	<link href="/css/app.css" rel="stylesheet">
